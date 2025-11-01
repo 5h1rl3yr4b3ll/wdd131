80a5f0a6b51dc039cb7e3fcace5b6d227eafe996
5h1rl3yr4b3ll <shirley.rabell@outlook.com>
2025-11-01 16:16:09 -0600

    W01 PROJECT

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// Set the current year in the footer
+const currentYear = new Date().getFullYear();
+// Note: The span is inside the paragraph in your HTML, so we just set the text content for the span.
+document.getElementById('currentyear').textContent = '©️${currentYear} 🐹 Shirley Anahi Rabell Magaña 🐹 Mexico City';
+
+// Set the last modified date in the footer
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = 'Last Modified: ${lastModified}';
